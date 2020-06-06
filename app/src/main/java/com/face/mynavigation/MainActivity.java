e65bca34734d68cd437fc6ca6154280cef6df393
zhonghzhang <cuit_zzh@163.com>
2020-06-06 18:12:10 +0800

    添加测试module

--- app/src/main/java/com/face/mynavigation/MainActivity.java
+++ app/src/main/java/com/face/mynavigation/MainActivity.java
@@ -2,6 +2,7 @@ package com.face.mynavigation;
 
 import android.os.Bundle;
 
+import com.face.myfirstlibrary.LibaryTest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        LibaryTest.print();
     }
 
 }
--- myfirstlibrary/src/main/java/com/face/myfirstlibrary/LibaryTest.java
+++ myfirstlibrary/src/main/java/com/face/myfirstlibrary/LibaryTest.java
@@ -1,4 +1,9 @@
 package com.face.myfirstlibrary;
 
+import android.util.Log;
+
 public class LibaryTest {
+    static public void print() {
+        Log.d(LibaryTest.class.getSimpleName(),"My first LibaryTest print -------->");
+    }
 }
